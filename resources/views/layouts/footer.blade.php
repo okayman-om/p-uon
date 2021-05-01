c315fa3d75d9a1794011b935a69a38a6eb8303d0
okayman.om <okayman.om@gmail.com>
2021-05-01 22:51:22 +1000

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="footer"></div>
+    <div class="copyright">Copyright © Group11 UON</div>
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- google-fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+
+    <!-- bootstrap-cdn -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+
+    <!-- customcss -->    
+    <link rel="stylesheet" href="css/custom.css">
+
+    <title>Uon</title>
+</head>
+<body>
+    <!-- ------------------------- Header ----------------------------------- -->
+    <div class="container">
+        <div class="head">
+            <a href="#"><img src="img/uon-logo-square.svg" alt="University of Newcastle" class="logo"></a>
+        </div>
+    </div>
+    <div class="navigation">
+        <div class="menu container">
+            <a href="index.html">Home</a>
+            <a href="thingstoknow.html">Things to know</a>
+            <a href="universitytools.html">University Tools</a>
+            <a href="videos.html">Videos</a>
+            <a href="blog.html">Blog</a>
+            <a href="faq.html">FAQ</a>
+        </div>
+    </div>
+    <!-- -------------------------------------- Header ends -----------------------------  -->
